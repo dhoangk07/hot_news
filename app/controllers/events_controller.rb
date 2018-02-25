5f114451193d54a5831e468a91303c8c3b622c6d
dhoang <dhoangk07@gmail.com>
2018-02-25 17:27:12 +0700

    As an admin, i would like to assign label to an article (final)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,22 +3,39 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+	def news
+		@event = Event.new
+	end
+
+	def show
+   @event = Event.find(params[:id])
+  end
+
+	def edit
+  	@event = Event.find(params[:id])
+  end
+
+  def update
+  	@event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      redirect_to root_path, notice: 'Updated article.'
+    else
+      render :edit
+    end
+  end
+
   def increase_view_count
   	@event = Event.find(params[:id])
   	respond_to do |format|
 	    format.js
 		  @event.view_count +=1
 		  @event.save
-	  end
+		end
   end
-
-	def news
-		@event = Event.new
-	end
 
 	private
 	def event_params
-    params.require(:event).permit(:title, :source, :tags_list)
+    params.require(:event).permit(:title, :source, :tag_list)
   end
 
 end
