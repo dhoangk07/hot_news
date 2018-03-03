4934f1ae442ce232e27bfaf7514b94f5a9cadef5
dhoang <dhoangk07@gmail.com>
2018-03-03 22:38:25 +0700

    Rspec Rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  
+  # for learning
+  unless Rails.env.test?
+    before_action :authenticate_user!
+  end
 end
