4e52477fb7a0850ab61566dd75c1f55f1f83d8b7
dhoang <dhoangk07@gmail.com>
2018-03-09 01:46:44 +0700

    Custom Devise view

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-    $(".hide-btn").click(function(event){
-      var target = $(event.target);
-      $(target).parents('.event-wrapper').hide();
-    });
+  $(".hide-btn").click(function(event){
+    var target = $(event.target);
+    $(target).parents('.event-wrapper').hide();
+    var id = target.attr('id');
+    var eventId = id.split('-')[1];
+    var url = "/events/" + eventId + "/hide";
+    $.ajax({
+      type: "GET",
+      url: url
+    })
+  });
 });
