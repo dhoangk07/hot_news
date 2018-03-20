217277db93b84d7325aa79b2c8423496ac658741
dhoang <dhoangk07@gmail.com>
2018-03-20 22:02:25 +0700

    make code easy to read

--- app/models/rsslink.rb
+++ app/models/rsslink.rb
@@ -2,9 +2,8 @@ class Rsslink < ApplicationRecord
   validates_uniqueness_of :source
 
   def self.rss_importer
-    link = Rsslink.all
-    link.map{|link| link.url}.each do |url|
-    RssCrawler.new("#{url}").run
+    Rsslink.each do |rsslink|
+      RssCrawler.new(rsslink.url).run
     end
   end
 end
