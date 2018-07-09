7e193f60072dd6f32868a05e1aafbd738377b734
HoangDinh <dhoangk07@gmail.com>
2018-07-09 23:02:39 +0700

    modify model rsslink

--- app/models/rsslink.rb
+++ app/models/rsslink.rb
@@ -2,8 +2,9 @@ class Rsslink < ApplicationRecord
   validates_uniqueness_of :source
 
   def self.rss_importer
-    Rsslink.each do |rsslink|
+    Rsslink.all.each do |rsslink|
       RssCrawler.new(rsslink.url).run
     end
   end
 end
+
