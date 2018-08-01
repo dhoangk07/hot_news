a84d4dd2afd3dbaf87513efe64eea56597d7f28a
HoangDinh <dhoangk07@gmail.com>
2018-08-01 23:28:07 +0700

    Add comment for Feedjira gem

--- app/lib/rss_crawler.rb
+++ app/lib/rss_crawler.rb
@@ -21,6 +21,33 @@ class RssCrawler
 
 end
 
-  
+# Feedjira gem
 
+# class Feedjira::Parser::RSSEntry
+#   element "google:elevation", as: :elevation
+# end
 
+# class RssCrawler
+#   def initialize(url)
+#     @url = url
+#   end
+
+#   def run
+#         debugger
+        
+#         Feedjira::Feed.add_common_feed_entry_element('ht:news_item')
+    # another solution
+    #feed = Feedjira::Feed.fetch_and_parse(@url)
+    # feed = Feedjira::Parser.json_feed_item(@url)
+    # feed = Feedjira::Parser::GoogleDocsAtomEntry.element(@url)
+#      xml = Faraday.get(@url).body
+#     feed = Feedjira::Feed.parse xml
+
+#     feed.entries.each do |entry|
+#       puts "Item: #{entry.title}"
+#       Event.create(title: entry.title, source: entry.url, date: Date.today)
+#     end 
+#     rescue => exception
+#     puts exception
+#   end
+# end
