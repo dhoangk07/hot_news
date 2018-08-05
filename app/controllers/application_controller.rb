3c9683e3e66d981456ddc9bf3b6b7c6afd94529c
HoangDinh <dhoangk07@gmail.com>
2018-08-06 01:03:51 +0700

    https://github.com/plataformatec/devise/wiki/How-To:-Create-custom-layouts
    https://trello.com/c/DE4pUU5E/51-when-im-login-page-the-page-should-not-show-up-the-login-button-and-sign-up-button-on-the-top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  layout :layout_by_resource
   # for learning
   unless Rails.env.test?
     before_action :authenticate_user!
+  end
+
+  private
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 end
