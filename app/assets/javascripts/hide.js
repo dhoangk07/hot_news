c489d25b08058b71b3362ab875a912798a7b26aa
HoangDinh <dhoangk07@gmail.com>
2018-08-27 00:18:42 +0700

    Fix function hide for event

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $(".hide-btn").click(function(event){
     var target = $(event.target);
-    $(target).parents('.event-wrapper').hide();
+    $(target).parents('.event-element').hide();
     var id = target.attr('id');
     var eventId = id.split('-')[1];
     var url = "/events/" + eventId + "/hide";
