3e8e305ed77d910af5c0425a43b7dfcf15fc3f64
HoangDinh <dhoangk07@gmail.com>
2018-09-09 11:47:45 +0700

    Wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { :host => "localhost:7000" }
   config.action_mailer.delivery_method = :letter_opener
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV['S3_REGION'],
+    s3_host_name: ENV['S3_HOST_NAME'],
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
